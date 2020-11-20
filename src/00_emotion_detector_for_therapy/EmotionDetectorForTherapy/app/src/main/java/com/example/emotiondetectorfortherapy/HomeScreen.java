8d39fbac9f882a7034022724afe0788c3fbcac4e
myusrilh <yusrilhasriansyah@gmail.com>
2020-11-20 08:28:36 +0700

    add databinding to project

--- src/00_emotion_detector_for_therapy/EmotionDetectorForTherapy/app/src/main/java/com/example/emotiondetectorfortherapy/HomeScreen.java
+++ src/00_emotion_detector_for_therapy/EmotionDetectorForTherapy/app/src/main/java/com/example/emotiondetectorfortherapy/HomeScreen.java
@@ -1,14 +1,21 @@
 package com.example.emotiondetectorfortherapy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.example.emotiondetectorfortherapy.databinding.ActivityHomeScreenBinding;
+
 public class HomeScreen extends AppCompatActivity {
+
+    ActivityHomeScreenBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_screen);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_home_screen);
+
+        mBinding.setNama("Hi, Yusril");
     }
 }
---
+++ src/00_emotion_detector_for_therapy/EmotionDetectorForTherapy/app/src/main/java/com/example/emotiondetectorfortherapy/viewmodel/SimpleViewModel.java
@@ -0,0 +1,4 @@
+package com.example.emotiondetectorfortherapy.viewmodel;
+
+public class SimpleViewModel {
+}
